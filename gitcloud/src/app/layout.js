95f27cd29543593788401cd68ba164845a42b3a4
SameerMatoria <sameermatoria@gmail.com>
2025-06-11 13:25:52 +0530

    vercel suspense fix-2

--- gitcloud/src/app/layout.js
+++ gitcloud/src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Suspense } from 'react';
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +30,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Suspense fallback={<div>Loading...</div>}>
+          {children}
+        </Suspense>
       </body>
     </html>
   );
